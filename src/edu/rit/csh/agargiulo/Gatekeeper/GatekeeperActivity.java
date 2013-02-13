e8db2bed84202adf4ff5b9ef3905b53fde62881f
Anthony Gargiulo <anthony@agargiulo.com>
2013-02-13 16:39:17 -0500

    Added the buttons for the user to unlock doors, as well as some code to update them based on the API calls to the gatekeeper server, among other minor edits

--- src/edu/rit/csh/agargiulo/Gatekeeper/GatekeeperActivity.java
+++ src/edu/rit/csh/agargiulo/Gatekeeper/GatekeeperActivity.java
@@ -1,13 +1,22 @@
 package edu.rit.csh.agargiulo.Gatekeeper;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author Anthony Gargiulo <anthony@agargiulo.com>
@@ -15,8 +24,74 @@ import android.view.MenuItem;
  */
 public class GatekeeperActivity extends Activity
 {
+	class AlertOnClickListener implements DialogInterface.OnClickListener
+	{
+
+		@Override
+		public void onClick (DialogInterface dialog, int which)
+		{
+			switch(which)
+			{
+			case DialogInterface.BUTTON_POSITIVE:
+				startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+			default:
+				break;
+			}
+		}
+
+	}
+
 	private HttpsConnector connector;
+
 	private SharedPreferences prefs;
+
+	private int getColorFromState (String doorState)
+	{
+		int color;
+		if(doorState.equals("unknown"))
+		{
+			color = Color.GRAY;
+		} else if(doorState.equals("unlocked"))
+		{
+			color = Color.RED;
+		} else if(doorState.equals("locked"))
+		{
+			color = Color.GREEN;
+		} else
+		{
+			color = Color.MAGENTA;
+		}
+		return color;
+	}
+
+	private int getId (int doorId)
+	{
+		switch(doorId)
+		{
+		/*
+		case 1:
+			return R.id.door_1;
+		case 2:
+			return R.id.door_2;
+		case 3:
+			return R.id.door_3;
+			*/
+		case 4:
+			return R.id.door_4;
+		case 5:
+			return R.id.door_5;
+		case 6:
+			return R.id.door_6;
+		case 7:
+			return R.id.door_7;
+		case 8:
+			return R.id.door_8;
+		case 9:
+			return R.id.door_9;
+		default:
+			return -1;
+		}
+	}
 
 	/**
 	 * Deletes the stored username and password from the preferences effectivly
@@ -44,7 +119,6 @@ public class GatekeeperActivity extends Activity
 		{
 			Log.d("gatekeeper", username);
 			invalidateOptionsMenu();
-			connector.getAllDoors();
 		}
 
 	}
@@ -58,8 +132,9 @@ public class GatekeeperActivity extends Activity
 		prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 		if(!prefs.getBoolean("loggedin", false))
 		{
-			startActivity(new Intent(this, LoginActivity.class));
+			startActivityForResult(new Intent(this, LoginActivity.class), 0);
 		}
+		connector.getAllDoors();
 	}
 
 	@Override
@@ -107,9 +182,87 @@ public class GatekeeperActivity extends Activity
 		return result;
 	}
 
-	public void update (String json)
+	public void popLock (View view)
 	{
-		Log.d("GatekeeperActivity.update(s): ", json);
+		int doorId;
+		switch(view.getId())
+		{
+		case R.id.door_4:
+			doorId = 4;
+			break;
+		case R.id.door_5:
+			doorId = 5;
+			break;
+		case R.id.door_6:
+			doorId = 6;
+			break;
+		case R.id.door_7:
+			doorId = 7;
+			break;
+		case R.id.door_8:
+			doorId = 8;
+			break;
+		case R.id.door_9:
+			doorId = 9;
+			break;
+		default:
+			doorId = -1;
+		}
+		connector.popDoor(doorId);
+
 	}
 
+	public void update (String jsonstr)
+	{
+		JSONObject obj;
+		int doorId;
+		String doorState, doorName;
+		Button tempButton;
+		AlertDialog.Builder dialogBuild;
+		AlertDialog dialog;
+		// RelativeLayout relLayout = (RelativeLayout)
+		// findViewById(R.id.gatekeeper_main_screen);
+		try
+		{
+			obj = new JSONObject(jsonstr);
+			Log.d("GatekeeperActivity.update(s): ", obj.toString(3));
+			if(obj.has("response") && !obj.getString("response").equals("null"))
+			{
+				// all_doors or door_state/id was called
+				JSONArray response = obj.getJSONArray("response");
+				for(int i = 0; i < response.length(); i ++)
+				{
+					doorState = response.getJSONObject(i).getString("state");
+					doorId = response.getJSONObject(i).getInt("id");
+					doorName = response.getJSONObject(i).getString("name");
+					tempButton = (Button) findViewById(getId(doorId));
+					tempButton.setText(doorName + ":" + doorState);
+					// tempButton.setBackground
+					// tempButton.setBackgroundColor(getColorFromState(doorState));
+					tempButton.setTextColor(getColorFromState(doorState));
+				}
+
+			} else
+			{
+				// We did a lock/pop/unlock opperation
+				if(obj.getString("success").equals("false"))
+				{
+					String errorMessage = obj.getString("error")
+							+ "\nGo back to the log in screen?";
+
+					dialogBuild = new AlertDialog.Builder(GatekeeperActivity.this).setTitle(
+							obj.getString("error_type")).setMessage(errorMessage);
+					dialogBuild.setPositiveButton("Yes, please", new AlertOnClickListener());
+					dialogBuild.setNegativeButton("No thanks", new AlertOnClickListener());
+					dialog = dialogBuild.create();
+					dialog.show();
+				}
+
+			}
+		} catch(JSONException je)
+		{
+			Log.e(this.getClass().toString(), je.getMessage(), je);
+			return;
+		}
+	}
 }
