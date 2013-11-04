ab0f8d65c98b2f40d71d8f83b3f25cb4ff2654bb
Anthony Gargiulo <anthony@agargiulo.com>
2013-11-04 17:33:05 -0500

    Added Google Analytics to the app

--- src/edu/rit/csh/agargiulo/Gatekeeper/AboutActivity.java
+++ src/edu/rit/csh/agargiulo/Gatekeeper/AboutActivity.java
@@ -3,17 +3,32 @@ package edu.rit.csh.agargiulo.Gatekeeper;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.analytics.tracking.android.EasyTracker;
+
 /**
  * @author Anthony Gargiulo <anthony@agargiulo.com>
  * 
  */
 public class AboutActivity extends Activity
 {
-
 	@Override
 	protected void onCreate (Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
+	}
+
+	@Override
+	public void onStart ()
+	{
+		super.onStart();
+		EasyTracker.getInstance().activityStart(this);
+	}
+
+	@Override
+	public void onStop ()
+	{
+		super.onStop();
+		EasyTracker.getInstance().activityStop(this);
 	}
 }
